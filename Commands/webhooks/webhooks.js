931e99c38372eeecfcb416b520c4337378c7b1fc
hugolino1 <hugolino1000@gmail.com>
2019-10-14 16:02:18 -0300

    Aviso de copyright adicionada em todos os arquivos
    Ação requerida pelo proprietário do bot principal e caso seja feita a redistribuição aprtir da minha versão também será necessário!

--- Commands/webhooks/webhooks.js
+++ Commands/webhooks/webhooks.js
@@ -1,3 +1,22 @@
+/*
+ This file is part of TrelloBot.
+
+ Copyright © Snazzah ??? - 2019
+ Copyright © Yamboy1 (and contributors) 2019
+ Copyright © Lobo Metalurgico 2019
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const { Command } = require("faux-classes");
 
 module.exports = class Webhooks extends Command {
@@ -24,7 +43,7 @@ module.exports = class Webhooks extends Command {
           return `Quadro ${webhook.board}\n  Bits: ${bits}`;
         }
       }, {
-        header: "Use `" + this.client.config.prefix + "webhooks [page]` para ver a lista.",
+        header: "Use `" + this.client.config.prefix + "webhooks [página]` para ver a lista.",
         pluralName: "Trello Webhooks",
         itemsPerPage: 3,
         startPage: args[0]
--- Core/EventHandler.js
+++ Core/EventHandler.js
@@ -1,3 +1,24 @@
+/*
+ This file is part of TrelloBot.
+
+ Copyright © Snazzah ??? - 2019
+ Copyright © Yamboy1 (and contributors) 2019
+ Copyright © Lobo Metalurgico 2019
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+
 const Util = require("./Util");
 
 module.exports = class EventHandler {
@@ -13,7 +34,7 @@ module.exports = class EventHandler {
     this.client.stats.bumpStat("messages");
     if (Message.author.bot) return;
     if (Message.channel.type !== "dm" && !Message.channel.permissionsFor(this.client.user).has("SEND_MESSAGES")) return;
-    if (Message.channel.type === "dm" && !this.client.elevated(Message)) return Message.reply("Não trabalho com mensagens privadas. Apenas os outros bots do lobo.");
+    if (Message.channel.type === "dm") return Message.reply("Não trabalho com mensagens privadas. Apenas os outros bots do lobo.");
 
     if (this.client.awaitedMessages.hasOwnProperty(Message.channel.id)
       && this.client.awaitedMessages[Message.channel.id].hasOwnProperty(Message.author.id)) {
